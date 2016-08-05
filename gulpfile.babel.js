fa2a7c17c3c9c5aa1eb3fda8c67d36e48b2f1438
大橋　勇 <y_ohashi@waku-2.com>
2016-08-05 18:59:27 +0900

    一旦コミット。

---
+++ gulpfile.babel.js
@@ -0,0 +1,75 @@
+import gulp from 'gulp';
+import browserify from 'browserify';
+import babelify from 'babelify';
+import BrowserSync from 'browser-sync';
+import nodemon from 'gulp-nodemon';
+import source from 'vinyl-source-stream';
+
+const browserSync = BrowserSync.create(),
+      reload = browserSync.reload;
+
+var path = {
+  js: {
+    app: './src/js/app.js',
+    src: './src/**/*.js',
+    dist: './public/js'
+  },
+  jsx: {
+    watch: './src/jsx/**/*.jsx'
+  }
+}
+
+gulp.task('babelify', () => {
+  browserify(path.js.app)
+    .transform(babelify)
+    .bundle()
+    .on('error', err => {
+      console.log(`Error: ${err.message}`);
+    })
+    .pipe(source('bundle.js'))
+    .pipe(gulp.dest(path.js.dist))
+    .pipe(reload({ stream: true }));
+});
+
+
+gulp.task('browser-sync', () => {
+  browserSync.init(null, {
+    server: {
+      baseDir: './'
+    },
+    port: 9000,
+    open: 'external'
+  });
+});
+
+
+gulp.task('nodemon', cb => {
+  let called = false;
+
+  return nodemon({
+    script: './app.js',
+    ext: 'js html',
+    ignore: [
+      './public',
+      './node_modules'
+    ]
+  })
+  .on('start', () => {
+    if (!called) {
+      called = true;
+      cb();
+    }
+  })
+  .on('restart', () => {
+    setTimeout(() => {
+      reload();
+    }, 500);
+  });
+});
+
+
+gulp.task('default', ['browser-sync'], () => {
+  gulp.watch('./public/index.html').on('change', reload);
+  gulp.watch(path.jsx.watch).on('change', reload);
+  gulp.watch(path.js.src, ['babelify']);
+});
